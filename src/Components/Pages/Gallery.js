31acf5f098835016669282eafdaefc058af9083f
Ivan <calintz1@hotmail.com>
2020-10-14 13:46:03 -0300

    ajustes de paginas

--- src/Components/Pages/Gallery.js
+++ src/Components/Pages/Gallery.js
@@ -7,17 +7,33 @@ export default function About() {
   return (
     <>
       <div className="gallery g-image">
-        <img alt="" src={require("../../Images/mario.jpg")} />
-        <img alt="" src={require("../../Images/mario1.jpg")} />
-        <img alt="" src={require("../../Images/mario2.jpg")} />
-
-        <img alt="" src={require("../../Images/mario3.jpg")} />
-        <img alt="" src={require("../../Images/mario4.jpg")} />
-        <img alt="" src={require("../../Images/mario55.jpg")} />
-
-        <img alt="" src={require("../../Images/mario6.jpg")} />
-        <img alt="" src={require("../../Images/mario7.jpg")} />
-        <img alt="" src={require("../../Images/mario8.jpg")} />
+        <div className="pic1">
+          <img alt="" src={require("../../Images/mario.jpg")} />
+        </div>
+        <div className="pic2">
+          <img alt="" src={require("../../Images/mario8.jpg")} />
+        </div>
+        <div className="pic3">
+          <img alt="" src={require("../../Images/mario3.jpg")} />
+        </div>
+        <div className="pic4">
+          <img alt="" src={require("../../Images/mario2.jpg")} />
+        </div>
+        <div className="pic5">
+          <img alt="" src={require("../../Images/mario4.jpg")} />
+        </div>
+        <div className="pic6">
+          <img alt="" src={require("../../Images/mario5.jpg")} />
+        </div>
+        <div className="pic7">
+          <img alt="" src={require("../../Images/mario6.jpg")} />
+        </div>
+        <div className="pic8">
+          <img alt="" src={require("../../Images/mario9.jpg")} />
+        </div>
+        <div className="pic9">
+          <img alt="" src={require("../../Images/mario1.jpg")} />
+        </div>
       </div>
       <Footer />
     </>
